0c2756216b1b3795ce4a5422a03997a7a65ce935
MayurKuchhadiya <mahermayur9@gmail.com>
2024-04-04 11:34:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Mayur",
+    author_email="mahermayur@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
